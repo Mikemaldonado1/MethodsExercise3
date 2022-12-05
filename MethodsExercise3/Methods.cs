8ff26bea239dd3d4bbc38e61529141b4bbc532ba
Michael Maldonado <MichaelFrankMaldonado@gmail.com>
2022-12-04 19:24:30 -0700

    Exercises

---
+++ MethodsExercise3/Methods.cs
@@ -0,0 +1,33 @@
+﻿/*
+Use as many descriptive names as possible. I do not want to see myInt1 - 100!
+For training purposes, most if not all the data for these methods can come from user input.
+Each one of these methods should be called from Main() and declared in the Program class.
+Be as creative as you want, the goal is to have working methods but if you want to make them look nice, go for it.
+As in Num 6 w/ age and voting method, you can say “You may vote” or “Looks like you're too young this time!” based on their age.
+This project should work in a manner where if someone forks/clones this from GitHub,
+they can just press the play button and have every method run in sequence, so all interface dialogue should be in place already.
+When finished, push this to GitHub!
+
+*/
+
+using System;
+namespace MethodsExercise3
+{
+	public class Methods
+	{
+		public void Answer(int numbers)
+		{
+			//•	Write a method that will print to the console all numbers 1000 through -1000
+
+			do
+			{
+				Console.WriteLine(numbers);
+				--numbers;
+
+			} while (numbers<= 1000 && numbers >=- 1000);
+
+
+        }
+    }
+}
+
--- MethodsExercise3/Program.cs
+++ MethodsExercise3/Program.cs
@@ -1,13 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection.Metadata;
 
 namespace MethodsExercise3
 {
-    class Program
+
+    public class Program
     {
+        //METHOD CALLS*****************************************
         static void Main(string[] args)
         {
 
+            //Print1000();
+            //Count3s();
+            //Equal();
+            //EvenOdd();
+            //PosOrNeg();
+            //Vote();
+            //Range1();
+            Table1();
         }
+
+
+        //PROGRAM*********************************************
+
+
+        //Print 1000 #'s******************************************
+        /*
+
+                       public static void Print1000()
+                       {
+
+                           for (int i = 1000; i >= -1000; i--)
+                           {
+                               Console.WriteLine(i);
+                           }
+                       }
+        */
+
+        //Print by 3's********************************************
+        /*  
+                      public static void Count3s()
+                      {
+
+                          for (int i = 3; i < 1000; i += 3)
+                          {
+                              Console.WriteLine(i);
+                          }
+
+                      }
+
+        */
+
+        //PRINT EQUALS*********************************************
+        /*
+
+        public static void Equal()
+        {
+            int num1 = 1;
+            int num2 = 1;
+
+            if (num1==num2)
+            {
+                Console.WriteLine("The numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("The nubmers are NOT equal");
+            }
+        }
+        */
+
+        /*
+        public static void EvenOdd()
+        {
+            int num1 = 2;
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("The number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+
+
+
+        }
+        */
+        /*
+                public static void PosOrNeg()
+                {
+                    int num1 = -1;
+
+                    if (num1 > 0)
+                    {
+                        Console.WriteLine("The number is Positive");
+                    }
+
+
+                    else
+                    {
+                        Console.WriteLine("The number is Negative");
+                    }
+                }
+
+        */
+
+
+        /*
+                public static void Vote()
+                {
+                    Console.WriteLine("What is your Age?");
+                    var Age = int.Parse(Console.ReadLine());
+
+                    if (Age >= 18)
+                    {
+                        Console.WriteLine("You Can Vote!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Sorry, You Cannot Vote :(");
+                    }
+                }
+        */
+        /*
+                public static void Range1()
+                {
+                    Console.WriteLine("Please enter an integer ");
+                    var response = int.Parse(Console.ReadLine());
+
+                    if (response <= -10 || response >= 10)
+                    {
+                        Console.WriteLine("The number is OUT of range");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is IN range");
+                    }
+
+                }
+        */
+
+        public static void Table1()
+        {
+            int num = 10;
+            int answer1;
+
+            for (int i = 1; i < 13; i++)
+            {
+                answer1 = i * num;
+                Console.WriteLine($"{i} X {num} = {answer1}");
+
+            }
+
+            
+            
+
+        }
+
     }
 }
+
+
+
+
+
+    
+
